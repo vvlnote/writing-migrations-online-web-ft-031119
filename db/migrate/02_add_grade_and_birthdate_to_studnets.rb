334a9beacba0a9464ca807e0352e28741c5779ae
Joy Lin <vvlnote@hotmail.com>
2019-04-22 22:29:47 +0000

    Done.

---
+++ db/migrate/02_add_grade_and_birthdate_to_studnets.rb
@@ -0,0 +1,8 @@
+class AddGradeAndBirthdateToStudnets < ActiveRecord::Migration[5.1]
+  
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+  end
+  
+end
