a6d1f747687407d49cac59fd3393ae96d80dcba4
Joy Lin <vvlnote@hotmail.com>
2019-04-22 22:18:13 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_studnets.rb
+++ db/migrate/02_add_grade_and_birthdate_to_studnets.rb
@@ -1,5 +1,8 @@
 class AddGradeBirthdateToStudnets < ActiveRecord::Migration[5.1]
   
-  
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+  end
   
 end
